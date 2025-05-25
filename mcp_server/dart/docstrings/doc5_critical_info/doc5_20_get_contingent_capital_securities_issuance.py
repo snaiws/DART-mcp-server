1f7aff82221388dcae506f7c9d7da2c710f51c83
GyuhoLee <lghmk2@gmail.com>
2025-05-26 07:03:54 +0900

    refac에 의한 변경

--- mcp_server/dart/docstrings/doc5_critical_info/doc5_20_get_contingent_capital_securities_issuance.py
+++ mcp_server/dart/docstrings/doc5_critical_info/doc5_20_get_contingent_capital_securities_issuance.py
@@ -2,13 +2,4 @@ from dataclasses import dataclass
 
 @dataclass
 class Docstring_get_contingent_capital_securities_issuance:
-    docstring = """Provides key information of Decision on Issuance of Amortizing Contingent Capital Securities, within the Major Event Report
-    
-Args:
-    api_key (str): API authentication key - Issued authentication key (40 characters)
-    corp_code (str): Unique code - Unique identification code of the company subject to disclosure (8 characters)
-    bgn_de (str): Start date (initial receipt date) - Search start receipt date (YYYYMMDD) ※ Information provided from 2015 onwards
-    end_de (str): End date (initial receipt date) - Search end receipt date (YYYYMMDD) ※ Information provided from 2015 onwards
-Return:
-    response (str): a message including information
-"""
+    docstring = """Provides key information of Decision on Issuance of Amortizing Contingent Capital Securities, within the Major Event Report"""
