5b43f960a6998a52ddb90185a8a984cc4d9b9723
lgh <lghmk2@gmail.com>
2025-05-26 17:26:21 +0900

    resolve #53: 1-3

---
+++ mcp_server/dart/callers/api1_disclosure_info/api1_3_get_disclosure.py
@@ -0,0 +1,80 @@
+import os
+import io
+import zipfile
+
+from apimanager import HttpxAPIManager
+
+
+
+async def get_disclosure(
+    base_url:str,
+    endpoint:str,
+    path_disclosures:str,
+    api_key:str, 
+    corp_code:str,
+    rcept_no:str,
+    )->list:
+
+    # API 요청 URL
+    params = {
+        "crtfc_key":api_key,
+        "rcept_no":rcept_no,
+    }
+    path_disclosure = os.path.join(path_disclosures, corp_code, f"{rcept_no}.xml")
+    os.makedirs(path_disclosure, exist_ok=True)
+
+    client = HttpxAPIManager(base_url)
+
+    # 클라이언트로 API 요청 보내기
+    response = await client.get(endpoint, params=params)
+    
+    # 응답 확인
+    if response.status_code == 200:
+        # 응답이 성공적인 경우 압축 파일 처리
+        zip_file = io.BytesIO(response.content)
+        # 압축 해제
+        with zipfile.ZipFile(zip_file) as z:
+            print("압축파일 내 파일 목록:")
+            for file_info in z.filelist:
+                xml_content = z.read(file_info.filename)
+            
+            # XML 파일 저장 (선택사항)
+            with open(path_disclosure, 'wb') as f:
+                f.write(xml_content)
+                
+    return [f"status[{response.status_code}]"]
+
+
+
+# 사용 예시
+if __name__ == "__main__":
+    # uv run -m dart.callers.api1_disclosure_info.api1_3_get_disclosure
+    import os
+    import asyncio
+
+    from dotenv import load_dotenv
+    load_dotenv(verbose=False)
+
+    async def test():
+        base_url = "https://opendart.fss.or.kr/api"
+        endpoint = "/document.xml"
+        path_disclosures = "."
+        API_KEY = os.getenv("DART_API_KEY")
+        corp_code = "00000000"
+        rcept_no = "20190401004781"
+        print(API_KEY)
+
+        results = await get_disclosure(
+            base_url = base_url,
+            endpoint = endpoint,
+            path_disclosures = path_disclosures,
+            api_key = API_KEY,
+            corp_code = corp_code,
+            rcept_no = rcept_no
+        )
+
+        for result in results:
+            print("---")
+            print(result)
+        
+    asyncio.run(test())
