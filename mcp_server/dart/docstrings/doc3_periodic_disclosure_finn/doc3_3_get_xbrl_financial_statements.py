cb626a982c11f480ffe02ace965f2911d8d43f31
lgh <lghmk2@gmail.com>
2025-05-27 19:54:43 +0900

    Update: 1-3 프롬프트 변경

--- mcp_server/dart/docstrings/doc3_periodic_disclosure_finn/doc3_3_get_xbrl_financial_statements.py
+++ mcp_server/dart/docstrings/doc3_periodic_disclosure_finn/doc3_3_get_xbrl_financial_statements.py
@@ -2,4 +2,4 @@ from dataclasses import dataclass
 
 @dataclass
 class Docstring_get_xbrl_financial_statements:
-    docstring = """Provides the original XBRL financial statement files(재무제표 원본파일) within periodic disclosure submitted by listed corporations (securities and KOSDAQ) and major unlisted corporations (subject to business report submission & IFRS application)."""
+    docstring = """Download the original XBRL financial statement files(재무제표 원본파일) within periodic disclosure submitted by listed corporations (securities and KOSDAQ) and major unlisted corporations (subject to business report submission & IFRS application)."""
