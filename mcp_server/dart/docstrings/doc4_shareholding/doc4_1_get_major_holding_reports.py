257361d992e2c52dfbc9acc2f798800d9c384c8b
lgh <lghmk2@gmail.com>
2025-05-29 14:24:22 +0900

    resolve #90, #91: 4-1, 4-2

---
+++ mcp_server/dart/docstrings/doc4_shareholding/doc4_1_get_major_holding_reports.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Docstring_get_major_holding_reports:
+    docstring = """[지분공시 종합정보 - 대량보유 상황보고]
+    Provides large shareholding status report information from the Report on Status of Large Shareholdings of Stocks and Other Securities."""
