6b2530782aaaabe1ea1d2e1ac65a3f1d076d2aa8
lgh <lghmk2@gmail.com>
2025-05-29 15:34:28 +0900

    resolve #84, #85, #86, #87, #88, #89: 6-1, 6-2, 6-3, 6-4, 6-5, 6-6

---
+++ mcp_server/dart/docstrings/doc6_securities_report/doc6_3_get_depositary_receipts_registration.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Docstring_get_depositary_receipts_registration:
+    docstring = """[증권신고서 주요정보 - 증권예탁증권]
+Provides summary information within Securities Registration Statement (Depositary Receipts)."""
