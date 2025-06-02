a4ef3d96e058f001b772babd49fa2733a4fa88b4
GyuhoLee <lghmk2@gmail.com>
2025-06-02 20:35:01 +0900

    resolve #72, #73, #74, #75: 3-5, 3-7, 3-8, 3-9

---
+++ mcp_server/dart/docstrings/doc5_critical_info/doc5_8_get_mixed_capital_increase.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Docstring_get_mixed_capital_increase:
+    docstring = """[주요사항보고서 주요정보 - 유무상증자 결정]
+    Provides key information of Decision on Paid-in Capital Increase and Stock Dividend, within the Material Event Report."""
---
+++ mcp_server/dart/docstrings/doc5_critical_info/doc5_9_get_capital_reduction.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Docstring_get_capital_reduction:
+    docstring = """[주요사항보고서 주요정보 - 감자 결정]
+    Provides key information of Decision on Capital Reduction, within the Material Event Report."""
