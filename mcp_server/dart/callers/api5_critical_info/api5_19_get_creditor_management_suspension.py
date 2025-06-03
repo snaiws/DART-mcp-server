be28e65b2d0c4985646d8440bc044642c884d219
이건호 <dlrjsgh367@gmail.com>
2025-06-03 16:45:34 +0900

    Feat(파일 복사): 5-19 - caller - docstring - schema

---
+++ mcp_server/dart/callers/api5_critical_info/api5_19_get_creditor_management_suspension.py
@@ -0,0 +1,55 @@
+from apimanager import HttpxAPIManager
+
+
+
+async def get_creditor_management_suspension(
+    base_url:str,
+    endpoint:str,
+    api_key:str, 
+    corp_code:str, 
+    bgn_de:str, 
+    end_de:str
+    )->list:
+    params = {
+        'crtfc_key': api_key,
+        'corp_code': corp_code,
+        'bgn_de': bgn_de,
+        'end_de': end_de
+    }
+
+    client = HttpxAPIManager(base_url)
+
+    # 클라이언트로 API 요청 보내기
+    response = await client.get(endpoint, params=params)
+    
+    # 후처리
+    data = response.json()
+    
+    transform1 = {
+        "rcept_no": "접수번호(14자리)",
+        "corp_cls": "법인구분",
+        "corp_code": "공시대상회사의 고유번호(8자리)",
+        "corp_name": "공시대상회사명",
+        "rp_rsn": "보고 사유",
+        "ast_inhtrf_prc": "자산양수·도 가액"
+    }
+
+    transform2 = {
+        "Y":"유가",
+        "K":"코스닥",
+        "N":"코넥스",
+        "E":"기타",
+    }
+    
+
+    result = []
+    for datum in data['list']:
+        # 법인구분처리
+        datum['corp_cls'] = transform2[datum['corp_cls']]
+        # dict to string
+        datum = [f"{transform1[k]}: {datum.get(k,'-')}" for k in transform1]
+        datum = "\n".join(datum)
+        result.append(datum)
+    return result
+
+    
---
+++ mcp_server/dart/docstrings/doc5_critical_info/doc5_19_get_creditor_management_suspension.py
@@ -0,0 +1,5 @@
+from dataclasses import dataclass
+
+@dataclass
+class Docstring_get_creditor_management_suspension:
+    docstring = """Provides key information of Asset Transfer(Other) & Put-back Option, within the Material Event Report."""
