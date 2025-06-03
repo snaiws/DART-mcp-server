7da9997a78d30dc51d1145179adb183f8d00b2cb
이건호 <dlrjsgh367@gmail.com>
2025-06-03 15:18:33 +0900

    Fix: docstring 수정

---
+++ mcp_server/dart/docstrings/doc5_critical_info/doc5_14_get_overseas_listing.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
+class Docstring_get_overseas_listing:
+    # author: @이건호
+    # Todo: 어색한 문장 검수 받기
+    docstring = """Provides key information within the Key Matters Report (Listing of Sovereign Securities, etc. in Overseas Securities Markets)."""
