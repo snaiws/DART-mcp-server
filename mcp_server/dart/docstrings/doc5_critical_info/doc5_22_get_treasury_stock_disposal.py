43196ff73d8b69998b48cb29ce9fc33eb52e92c5
lgh <lghmk2@gmail.com>
2025-06-06 12:02:59 +0900

    Update: docstring 수정

--- mcp_server/dart/docstrings/doc5_critical_info/doc5_22_get_treasury_stock_disposal.py
+++ mcp_server/dart/docstrings/doc5_critical_info/doc5_22_get_treasury_stock_disposal.py
@@ -2,5 +2,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Docstring_get_treasury_stock_disposal:
-    docstring = """"[주요사항보고서 주요정보 - 자기주식 처분 결정]
+    docstring = """[주요사항보고서 주요정보 - 자기주식 처분 결정]
     Provides key information of Decision on Treasury Stock Disposal, within the Major Event Report"""
