75d53fdcb23f4d75ae72c77e8f1cf8aa65478c99
lgh <lghmk2@gmail.com>
2025-06-09 19:42:06 +0900

    Bugfix: 환경변수load시점변경

--- mcp_server/configs/path.py
+++ mcp_server/configs/path.py
@@ -2,15 +2,14 @@ import os
 from dataclasses import dataclass, field
 from pathlib import Path
 
-path_data = os.getenv("PATH_DATA", ".")
 
 @dataclass
 class PathDefine:
     '''
     파이썬 환경변수
     '''
-    PATH_CORPLIST: Path = field(default_factory=lambda: Path(path_data) / 'CORPCODE.xml')
-    PATH_FINSTATS: Path = field(default_factory=lambda: Path(path_data) / 'finstats')
-    PATH_DISCLOSURES: Path = field(default_factory=lambda: Path(path_data) / 'disclosures')
-    PATH_LOG: Path = field(default_factory=lambda: Path(path_data) / 'dart.log')
+    PATH_CORPLIST: Path = field(default_factory=lambda: Path(os.getenv("PATH_DATA", "./data/mcp/DART/")) / 'CORPCODE.xml')
+    PATH_FINSTATS: Path = field(default_factory=lambda: Path(os.getenv("PATH_DATA", "./data/mcp/DART/")) / 'finstats')
+    PATH_DISCLOSURES: Path = field(default_factory=lambda: Path(os.getenv("PATH_DATA", "./data/mcp/DART/")) / 'disclosures')
+    PATH_LOG: Path = field(default_factory=lambda: Path(os.getenv("PATH_DATA", "./data/mcp/DART/")) / 'dart.log')
     
