bf162cbd63b88578a67eb16c62bdb714bb4f5d91
lgh <lghmk2@gmail.com>
2025-06-10 13:24:22 +0900

    Merge: 5-13, 5-14, 5-19

--- mcp_server/dart/docstrings/doc5_critical_info/doc5_14_get_overseas_listing.py
+++ mcp_server/dart/docstrings/doc5_critical_info/doc5_14_get_overseas_listing.py
@@ -4,4 +4,5 @@ from dataclasses import dataclass
 class Docstring_get_overseas_listing:
     # author: @이건호
     # Todo: 어색한 문장 검수 받기
-    docstring = """Provides key information within the Key Matters Report (Listing of Sovereign Securities, etc. in Overseas Securities Markets)."""
+    docstring = """[주요사항보고서 주요정보 - 해외 증권시장 주권등 상장]
+    Provides key information of Listing of Sovereign Securities, etc. in Overseas Securities Markets, within the Key Matters Report."""
--- mcp_server/dart/docstrings/doc5_critical_info/doc5_19_get_creditor_management_suspension.py
+++ mcp_server/dart/docstrings/doc5_critical_info/doc5_19_get_creditor_management_suspension.py
@@ -2,4 +2,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Docstring_get_creditor_management_suspension:
-    docstring = """Provides key information regarding termination of management procedures by bond banks and other institutions within the Major Event Report."""
+    docstring = """[주요사항보고서 주요정보 - 채권은행 등의 관리절차 중단]
+    Provides key information regarding termination of management procedures by bond banks and other institutions within the Major Event Report."""
