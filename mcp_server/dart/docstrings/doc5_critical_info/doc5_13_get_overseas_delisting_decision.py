64966b07908b397649b0fd70bc0ccea3c450153c
lgh <lghmk2@gmail.com>
2025-06-10 13:12:53 +0900

    Merge: 5-13

--- mcp_server/dart/docstrings/doc5_critical_info/doc5_13_get_overseas_delisting_decision.py
+++ mcp_server/dart/docstrings/doc5_critical_info/doc5_13_get_overseas_delisting_decision.py
@@ -2,4 +2,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Docstring_get_overseas_delisting_decision:
-    docstring = """Provides key information of Decision to Delist Shares from an Overseas Securities Market, from the Material Disclosure Report."""
+    docstring = """[주요사항보고서 주요정보 - 해외 증권시장 주권등 상장폐지 결정]
+    Provides key information of Decision to Delist Shares from an Overseas Securities Market, from the Material Disclosure Report."""
