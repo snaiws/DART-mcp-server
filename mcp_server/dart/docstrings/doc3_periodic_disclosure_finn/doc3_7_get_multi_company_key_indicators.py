a3eeac77c7a696ba70119179b62ee06ed0d49bd6
lgh <lghmk2@gmail.com>
2025-06-10 13:06:21 +0900

    Merge: 3-7

--- mcp_server/dart/docstrings/doc3_periodic_disclosure_finn/doc3_7_get_multi_company_key_indicators.py
+++ mcp_server/dart/docstrings/doc3_periodic_disclosure_finn/doc3_7_get_multi_company_key_indicators.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Docstring_get_multi_company_key_indicators:
-    docstring = """Provide key financial indicators extracted from XBRL financial statements in periodic reports filed by listed companies (KRX Securities and KOSDAQ) and major unlisted companies (required to file business reports & applying IFRS).
+    docstring = """[정기보고서 재무정보 - 다중회사 주요 재무지표]
+    Provide key financial indicators extracted from XBRL financial statements in periodic reports filed by listed companies (KRX Securities and KOSDAQ) and major unlisted companies (required to file business reports & applying IFRS).
     This is much better when comparing multiple companies.
     """
